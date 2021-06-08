ad4167d55e7061c2ecc0a67890d481015d5ed1f5
increpo <increpo@gmail.com>
2021-06-08 20:06:23 +0500

    added animal class

--- main.php
+++ main.php
@@ -1,1 +1,18 @@
 <?php
+
+class Animal{
+    private $name;
+    public $id;
+
+    public function __construct($name)
+    {
+    		$this->id = substr(md5(rand()), 0, 6);
+        $this->name = $name;
+    }
+
+    public function getProduct($minProduct, $maxProduct): int
+    {
+        return rand($minProduct, $maxProduct);
+    }
+}
+
